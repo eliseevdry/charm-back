bcd2e01e9c95152bd2140e367141036c8d978d2d
Andrey Eliseev <eliseevdry@mail.ru>
2024-07-26 14:45:40 +0300

    CHARM-5: HTTPClient + Postman

--- src/ru/eliseev/charm/back/CharmBackClientRunner.java
+++ src/ru/eliseev/charm/back/CharmBackClientRunner.java
@@ -1,24 +1,25 @@
 package ru.eliseev.charm.back;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.stream.Collectors;
 
 public class CharmBackClientRunner {
-    public static void main(String[] args) throws IOException {
-        try (Socket socket = new Socket("localhost", 8080);
-             DataOutputStream requestStream = new DataOutputStream(socket.getOutputStream());
-             DataInputStream responseStream = new DataInputStream(socket.getInputStream());
-             Scanner scanner = new Scanner(System.in)
-        ) {
-            while (scanner.hasNextLine()) {
-                String request = scanner.nextLine();
-                requestStream.writeUTF(request);
-                String response = responseStream.readUTF();
-                System.out.println(response);
-            }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try (HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://yandex.ru"))
+                    .GET()
+                    .build();
+            HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            HttpClient.Version version = response.version();
+            int statusCode = response.statusCode();
+            String headers = response.headers().map().entrySet().stream().map(Object::toString).collect(Collectors.joining("\n"));
+            String body = new String(response.body());
+            System.out.println(version + " " + statusCode + "\n" + headers + "\n\n" + body);
         }
     }
 }
