7730da3dcea71a26238d70dc69d84dc5511bc729
Andrey Eliseev <eliseevdry@mail.ru>
2024-08-11 16:31:22 +0300

    CHARM-9: Servlet

--- src/ru/eliseev/charm/back/controller/LikeController.java
+++ src/ru/eliseev/charm/back/controller/LikeController.java
@@ -1,19 +1,43 @@
 package ru.eliseev.charm.back.controller;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ru.eliseev.charm.back.service.LikeService;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/like")
 public class LikeController extends HttpServlet {
 
+    private final LikeService service = LikeService.getInstance();
+
+    @Override
+    public void init(ServletConfig config) {
+        System.out.println("Servlet started, name: " + config.getServletName());
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().write("10");
+        long id = Long.parseLong(req.getParameter("id"));
+
+        try (PrintWriter writer = resp.getWriter()) {
+            resp.setContentType("text/html");
+            resp.setCharacterEncoding("UTF-8");
+            writer.write("<h2>");
+            writer.write("<p> RequestURI: " + req.getRequestURI() + "</p>");
+            writer.write("<p> User agent header: " + req.getHeader("User-Agent") + "</p>");
+            writer.write("<p> Likes count: " + service.getLikesByProfileId(id) + "</p>");
+            writer.write("</h2>");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet destroyed");
     }
 }
