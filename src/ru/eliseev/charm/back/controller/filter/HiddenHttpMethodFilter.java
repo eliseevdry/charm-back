52353e3ae7f6e4e90cc6aa0e41759ebe89482f56
Andrey Eliseev <eliseevdry@mail.ru>
2024-08-21 23:09:59 +0300

    CHARM-12: Filters

---
+++ src/ru/eliseev/charm/back/controller/filter/HiddenHttpMethodFilter.java
@@ -0,0 +1,68 @@
+package ru.eliseev.charm.back.controller.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.eliseev.charm.back.model.Gender;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Locale;
+
+@WebFilter("/*")
+public class HiddenHttpMethodFilter implements Filter {
+
+    private static final String METHOD_PARAM = "_method";
+    
+    @Override
+    public void init(FilterConfig filterConfig) {
+        ServletContext servletContext = filterConfig.getServletContext();
+        if (servletContext.getAttribute("genders") == null) {
+            servletContext.setAttribute("genders", Gender.values());
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+            throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String paramValue = request.getParameter(METHOD_PARAM);
+        
+        if ("POST".equals(request.getMethod()) && paramValue != null && !paramValue.isBlank()) {
+            String method = paramValue.toUpperCase(Locale.ENGLISH);
+            HttpServletRequest wrapper = new HttpMethodRequestWrapper(request, method);
+            filterChain.doFilter(wrapper, response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    /**
+     * Simple {@link HttpServletRequest} wrapper that returns the supplied
+     * method for {@link HttpServletRequest#getMethod()}.
+     */
+    private static class HttpMethodRequestWrapper extends HttpServletRequestWrapper {
+
+        private final String method;
+
+        public HttpMethodRequestWrapper(HttpServletRequest request, String method) {
+            super(request);
+            this.method = method;
+        }
+
+        @Override
+        public String getMethod() {
+            return this.method;
+        }
+    }
+}
