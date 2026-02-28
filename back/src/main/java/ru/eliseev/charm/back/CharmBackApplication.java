eab6676459a90a37f63f0aec4d93c7aa7245e8c3
Andrey Eliseev <eliseevdry@mail.ru>
2026-02-28 14:32:40 +0300

    executable jar (dependencies)

--- back/src/main/java/ru/eliseev/charm/back/CharmBackApplication.java
+++ back/src/main/java/ru/eliseev/charm/back/CharmBackApplication.java
@@ -7,13 +7,20 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.archiver.jar.JarUnArchiver;
 import ru.eliseev.charm.back.config.CharmServletContainerInitializer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
+
+import static ru.eliseev.charm.back.utils.UrlUtils.WEBAPP_DIR;
 
 @Slf4j
 public class CharmBackApplication {
@@ -24,8 +31,8 @@ public class CharmBackApplication {
 
     @SneakyThrows
     private void start() {
-        Path webappDir = Paths.get(getClass().getClassLoader().getResource("webapp").toURI());
         Path tmpBaseDir = Files.createTempDirectory("tomcat-base-dir");
+        String webappDir = getWebappDir(tmpBaseDir);
 
         Tomcat tomcat = new Tomcat();
 
@@ -33,7 +40,7 @@ public class CharmBackApplication {
         tomcat.setPort(8080);
 
         tomcat.getConnector();
-        Context ctx = tomcat.addWebapp("", webappDir.toString());
+        Context ctx = tomcat.addWebapp("", webappDir);
         ctx.addServletContainerInitializer(new CharmServletContainerInitializer(), getWebClasses());
 
         tomcat.start();
@@ -50,5 +57,29 @@ public class CharmBackApplication {
             }
         }
         return classes;
+    }
+
+    @SneakyThrows
+    private String getWebappDir(Path tempDir) {
+        URL webappResource = getClass().getClassLoader().getResource(WEBAPP_DIR);
+        String webappResourcePath = webappResource.getPath();
+        log.info("webapp path: {}", webappResourcePath);
+        if (webappResource.getProtocol().equals("jar")) {
+            UnArchiver unArchiver = new JarUnArchiver();
+            unArchiver.setSourceFile(getJarFile(webappResource));
+            unArchiver.extract(WEBAPP_DIR, tempDir.toFile());
+            return tempDir.resolve(WEBAPP_DIR).toString();
+        } else {
+            return webappResourcePath;
+        }
+    }
+
+    private File getJarFile(URL webappResource) {
+        try {
+            JarURLConnection urlConnection = (JarURLConnection) webappResource.openConnection();
+            return new File(urlConnection.getJarFileURL().getPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- back/src/main/java/ru/eliseev/charm/back/utils/UrlUtils.java
+++ back/src/main/java/ru/eliseev/charm/back/utils/UrlUtils.java
@@ -20,6 +20,7 @@ public class UrlUtils {
     public static final String CONTENT_URL = "/content";
     public static final String PDF_URL = "/pdf";
     public static final String REST_URL = "/api/v1";
+    public static final String WEBAPP_DIR = "webapp/";
     public static final String LOGIN_REST_URL = REST_URL + LOGIN_URL;
     public static final String BASE_CONTENT_PATH = ConfigFileUtils.get("app.base-content-path");
 
