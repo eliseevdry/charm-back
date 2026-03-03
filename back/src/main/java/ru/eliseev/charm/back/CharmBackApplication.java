fb25d66040b4acab3a02fcd5fa296c19cb2927e3
Andrey Eliseev <eliseevdry@mail.ru>
2026-03-03 21:45:57 +0300

    CHARM-54: Executable jar

--- back/src/main/java/ru/eliseev/charm/back/CharmBackApplication.java
+++ back/src/main/java/ru/eliseev/charm/back/CharmBackApplication.java
@@ -4,14 +4,25 @@ import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ScanResult;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.archiver.jar.JarUnArchiver;
 import ru.eliseev.charm.back.config.CharmServletContainerInitializer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
+import static ru.eliseev.charm.back.utils.UrlUtils.WEBAPP_DIR;
+
+@Slf4j
 public class CharmBackApplication {
     public static void main(String[] args) {
         new CharmBackApplication().start();
@@ -19,13 +30,16 @@ public class CharmBackApplication {
 
     @SneakyThrows
     private void start() {
+        Path tmpBaseDir = Files.createTempDirectory("tomcat-base-dir");
+        String webappDir = getWebappDir(tmpBaseDir);
+
         Tomcat tomcat = new Tomcat();
         tomcat.setBaseDir(Files.createTempDirectory("tomcat-base").toString());
         tomcat.setPort(8080);
 
         tomcat.getConnector();
 
-        Context context = tomcat.addWebapp("", getClass().getClassLoader().getResource("webapp/").getPath());
+        Context context = tomcat.addWebapp("", webappDir);
 
         context.addServletContainerInitializer(new CharmServletContainerInitializer(), getWebClasses());
 
@@ -43,5 +57,29 @@ public class CharmBackApplication {
             }
         }
         return classes;
+    }
+
+    @SneakyThrows
+    private String getWebappDir(Path tempDir) {
+        URL webappResource = getClass().getClassLoader().getResource(WEBAPP_DIR);
+        String webappResourcePath = webappResource.getPath();
+        log.info("webapp path: {}", webappResourcePath);
+        if (webappResource.getProtocol().equals("jar")) {
+            UnArchiver unArchiver = new JarUnArchiver();
+            unArchiver.setSourceFile(getJarFile(webappResource));
+            unArchiver.extract(WEBAPP_DIR, tempDir.toFile());
+            return tempDir.resolve(WEBAPP_DIR).toString();
+        } else {
+            return webappResourcePath;
+        }
+    }
+
+    private File getJarFile(URL webappResource) {
+        try {
+            JarURLConnection urlConnection = (JarURLConnection) webappResource.openConnection();
+            return new File(urlConnection.getJarFileURL().getPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
