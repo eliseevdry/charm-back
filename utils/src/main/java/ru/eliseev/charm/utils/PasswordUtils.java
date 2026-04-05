754920c387d0c7832f94bd34b73f6b18ef7cca45
Andrey Eliseev <eliseevdry@mail.ru>
2026-04-05 21:24:24 +0300

    CHARM-58: TDD, AssertJ

--- utils/src/main/java/ru/eliseev/charm/utils/PasswordUtils.java
+++ utils/src/main/java/ru/eliseev/charm/utils/PasswordUtils.java
@@ -3,12 +3,15 @@ package ru.eliseev.charm.utils;
 import lombok.experimental.UtilityClass;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.util.Objects;
+
 import static ru.eliseev.charm.utils.StringUtils.isBlank;
 
 @UtilityClass
 public class PasswordUtils {
 
     public static String hashPassword(String plainPassword) {
+        Objects.requireNonNull(plainPassword, "Must not be null");
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
--- utils/src/test/java/ru/eliseev/charm/utils/PasswordUtilsTest.java
+++ utils/src/test/java/ru/eliseev/charm/utils/PasswordUtilsTest.java
@@ -3,19 +3,45 @@ package ru.eliseev.charm.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class PasswordUtilsTest {
     @Test
-    @DisplayName("Когда пароль захеширован - он не должен быть равен исходному")
+    @DisplayName("Когда пароль захеширован за 100 мс и он не должен быть равен исходному")
     void hashPassword1() {
         // given
         String password = "qwerty";
 
         // when
-        String result = PasswordUtils.hashPassword(password);
+        String result = assertDoesNotThrow(() -> assertTimeout(Duration.ofMillis(100), () -> PasswordUtils.hashPassword(password)));
 
         // then
-        assertNotEquals(password, result, "Ошибка: пароль не был захеширован");
+        assertThat(result)
+            .isNotNull()
+            .isNotEqualTo(password)
+            .hasSizeGreaterThan(password.length());
+    }
+
+    @Test
+    @DisplayName("Когда пароль null - он должен выбрасывать исключение")
+    void hashPassword2() {
+        // given
+        String password = null;
+
+        // when
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> PasswordUtils.hashPassword(password));
+
+        // then
+        assertThat(exception)
+            .isNotNull()
+            .satisfies(
+                ex -> assertThat(ex.getMessage()).isNotNull(),
+                ex -> assertThat(ex.getMessage()).isEqualTo("Must not be null")
+            );
     }
 }
