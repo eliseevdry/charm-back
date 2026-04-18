1791cad213db75197141ba9a534f9d436384a975
Andrey Eliseev <eliseevdry@mail.ru>
2026-04-18 17:02:49 +0300

    CHARM-59: Flaky tests

--- utils/src/main/java/ru/eliseev/charm/utils/PasswordUtils.java
+++ utils/src/main/java/ru/eliseev/charm/utils/PasswordUtils.java
@@ -1,18 +1,25 @@
 package ru.eliseev.charm.utils;
 
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.security.DrbgParameters;
+import java.security.SecureRandom;
 import java.util.Objects;
 
+import static java.security.DrbgParameters.Capability.RESEED_ONLY;
 import static ru.eliseev.charm.utils.StringUtils.isBlank;
 
 @UtilityClass
 public class PasswordUtils {
 
+    @SneakyThrows
     public static String hashPassword(String plainPassword) {
         Objects.requireNonNull(plainPassword, "Must not be null");
-        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+        SecureRandom random = SecureRandom.getInstance("DRBG",
+            DrbgParameters.instantiation(128, RESEED_ONLY, null));
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt(random.nextInt(3, 12), random));
     }
 
     public static boolean checkPassword(String plainPassword, String hashedPassword) {
